c82fa467ba2191a22f4a6fee738f130616075924
Prabhu Suriya <prabhusuriya.chennai@gmail.com>
2022-08-05 10:03:14 +0530

    api redirect

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseAPIUrl: 'https://www.vrbo.com',
+  baseAPIUrl: 'https://prop-view.prabhusuriya.com',
   MAP_API_KEY: 'AIzaSyD_wN5jb0lnirYmRihz1z7KtAqTZsSWQiI',
   MAP_API_URL: 'https://maps.googleapis.com/maps/api/js' // TODO move to build secrets. Committing anyway for dev purpose
 };
