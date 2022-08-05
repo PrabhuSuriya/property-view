7174201d5073eb51a7412b78d87aaee7f980989c
Prabhu Suriya <prabhusuriya.chennai@gmail.com>
2022-08-05 08:57:35 +0530

    update env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
   baseAPIUrl: 'https://www.vrbo.com',
+  MAP_API_KEY: 'AIzaSyD_wN5jb0lnirYmRihz1z7KtAqTZsSWQiI',
+  MAP_API_URL: 'https://maps.googleapis.com/maps/api/js' // TODO move to build secrets. Committing anyway for dev purpose
 };
